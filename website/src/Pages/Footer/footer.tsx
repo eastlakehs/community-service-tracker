90c68613488f85a7bd7cebfe429a51390886990e
Jason Zhang <zjjc123@hotmail.com>
2020-08-12 11:37:40 -0700

    Style Change

--- website/src/Pages/Footer/footer.tsx
+++ website/src/Pages/Footer/footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-white mt-24">
-            <p className="text-gray-700 text-center py-2">Eastlake 2020</p>
+        <footer className="bg-top-red mt-24">
+            <p className="text-white text-center py-2">Eastlake 2020</p>
         </footer>
     );
 };
