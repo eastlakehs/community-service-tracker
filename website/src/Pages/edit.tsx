020e6495b9ffc4a8568bc62d0ce49182ccc90f90
Jason Zhang <zjjc123@hotmail.com>
2020-08-28 11:49:14 -0700

    Date Picker

--- website/src/Pages/edit.tsx
+++ website/src/Pages/edit.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { StringField, CheckBox } from "../Components/Entry/entry";
 import Helmet from "../Components/Header/helmet";
 import PageHeader from "../Components/Header/pageHeader";
 import Footer from "../Components/Footer/footer";
+
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import DatePicker, { DayValue } from 'react-modern-calendar-datepicker';
 
 const FormSubmitButton: React.FC<{}> = () => {
   return (
@@ -14,6 +17,8 @@ const FormSubmitButton: React.FC<{}> = () => {
 };
 
 const Edit: React.FC<{}> = () => {
+  const [selectedDay, setSelectedDay] = useState<DayValue>(null);
+
   return (
     <div className="flex flex-col min-h-screen bg-eastlake-grey font-text">
       <Helmet
@@ -31,7 +36,17 @@ const Edit: React.FC<{}> = () => {
             placeholder="Orginization, club, etc."
           />
           <StringField name="Description" placeholder="I did ....." />
-          <StringField name="Date" placeholder="December-1-2020" />
+
+          <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
+            Date
+            </label>
+          <DatePicker
+            value={selectedDay}
+            onChange={setSelectedDay}
+            inputPlaceholder="Select a day"
+            inputClassName="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          />
+
           <StringField name="Hours" placeholder="Ex: 5" />
           <StringField name="Contact Name" placeholder="Jonathan Swift" />
           <StringField name="Contact Phone Number" placeholder="(425) 123-4567" />
