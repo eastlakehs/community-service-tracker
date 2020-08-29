102528a715506be1f5cae6de4a6b16fd41767582
Jason Zhang <zjjc123@hotmail.com>
2020-08-29 15:03:03 -0700

    Correcting Name

--- website/src/Components/Profile/profile.tsx
+++ website/src/Components/Profile/profile.tsx
@@ -9,7 +9,7 @@ const Profile: React.FunctionComponent<{}> = () => {
         <div className="mb-auto">
             <form className="w-full max-w-lg container mx-auto px-4 sm:px-8 items-center">
                 <StringField name="First Name" placeholder="John" value="" setValue={setValue} />
-                <StringField name="First Name" placeholder="Smith" value="" setValue={setValue} />
+                <StringField name="Last Name" placeholder="Smith" value="" setValue={setValue} />
                 <StringField name="Graduation Year" placeholder="2022" value="" setValue={setValue} />
                 <FormSubmitButton
                     buttonText={"Save Profile"}
