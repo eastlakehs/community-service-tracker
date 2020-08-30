bbda412d280389fa6028cf3200d396ddaee891bb
Jason Zhang <zjjc123@hotmail.com>
2020-08-29 19:06:39 -0700

    Updating Edit Validation

--- website/src/Components/Edit/validation.test.ts
+++ website/src/Components/Edit/validation.test.ts
@@ -1,4 +1,4 @@
-import { VALIDATE_hours, VALIDATE_date } from "./validation";
+import { VALIDATE_hours } from "./validation";
 
 test("Running Validation Tests for VALIDATE_hours", () => {
   expect(VALIDATE_hours(".")).toBe(false);
@@ -11,17 +11,6 @@ test("Running Validation Tests for VALIDATE_hours", () => {
   expect(VALIDATE_hours("1.5")).toBe(true);
   expect(VALIDATE_hours("20.1")).toBe(true);
   expect(VALIDATE_hours("20.8")).toBe(true);
-});
-
-test("Running Validation Tests for VALIDATE_date", () => {
-  expect(VALIDATE_date("12345678")).toBe(false);
-  expect(VALIDATE_date("12/06/20")).toBe(true);
-  expect(VALIDATE_date("0.5")).toBe(true);
-  expect(VALIDATE_date(".05")).toBe(true);
-  expect(VALIDATE_date("0ajsf.")).toBe(false);
-  expect(VALIDATE_date("dfsjaf")).toBe(false);
-  expect(VALIDATE_date("20")).toBe(true);
-  expect(VALIDATE_date("1.5")).toBe(true);
-  expect(VALIDATE_date("20.1")).toBe(true);
-  expect(VALIDATE_date("20.8")).toBe(true);
+  expect(VALIDATE_hours("0")).toBe(true);
+  expect(VALIDATE_hours("1.0")).toBe(true);
 });
--- website/src/Components/Edit/validation.ts
+++ website/src/Components/Edit/validation.ts
@@ -12,56 +12,12 @@ const VALIDATE_hours = (hours: string) => {
       if (dot_used) return false;
       else dot_used = true;
     } else if (
-      !(hours.charAt(i) in ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+      !(hours.charAt(i) in ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
     ) {
       return false;
     }
   }
   return true;
 };
-/** Returns true if the input string is a valid date. Function is safe regardless of input type.
- *  Format: MM-DD-YY
- */
-const VALIDATE_date = (date: string) => {
-  if (typeof date !== "string") return false;
-  if (date.length !== 8) return false;
-  // START Month CHECK
-  if (
-    !(
-      date.charAt(0) + date.charAt(1) in
-      ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    )
-  ) {
-    return false;
-  }
-  // Start slash check
-  if (date.charAt(2) !== "/") return false;
-  // Start Day Check
-  if (
-    date.charAt(3) === "0" &&
-    !(date.charAt(4) in ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-  ) {
-    return false;
-  }
-  if (
-    date.charAt(3) in ["1", "2"] &&
-    !(date.charAt(4) in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-  ) {
-    return false;
-  }
-  if (date.charAt(3) === "3" && !(date.charAt(4) in [0, 1])) {
-    return !false;
-  }
-  // Start Slash Check
-  if (date.charAt(5) !== "/") return false;
-  // Start Year Check
-  if (
-    date.charAt(6) in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] &&
-    !(date.charAt(7) in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-  ) {
-    return false;
-  }
-  return true;
-};
 
-export { VALIDATE_hours, VALIDATE_date };
+export { VALIDATE_hours };
