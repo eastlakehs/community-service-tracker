e5bc5a8340c014ed0986bff2f0a4efcd445e1b98
Jason Zhang <zjjc123@hotmail.com>
2020-08-30 15:08:19 -0700

    Validation Message Interface

--- website/src/Components/Edit/validation.test.ts
+++ website/src/Components/Edit/validation.test.ts
@@ -1,30 +1,30 @@
 import { VALIDATE_hours, VALIDATE_free_form } from "./validation";
 
 test("Running Validation Tests for VALIDATE_hours", () => {
-  expect(VALIDATE_hours(".5")).toBe(true);
-  expect(VALIDATE_hours("0.5")).toBe(true);
-  expect(VALIDATE_hours(".05")).toBe(true);
-  expect(VALIDATE_hours("20")).toBe(true);
-  expect(VALIDATE_hours("1.5")).toBe(true);
-  expect(VALIDATE_hours("20.1")).toBe(true);
-  expect(VALIDATE_hours("20.8")).toBe(true);
-  expect(VALIDATE_hours("0")).toBe(true);
-  expect(VALIDATE_hours("1.0")).toBe(true);
+  expect(VALIDATE_hours(".5").validate).toBe(true);
+  expect(VALIDATE_hours("0.5").validate).toBe(true);
+  expect(VALIDATE_hours(".05").validate).toBe(true);
+  expect(VALIDATE_hours("20").validate).toBe(true);
+  expect(VALIDATE_hours("1.5").validate).toBe(true);
+  expect(VALIDATE_hours("20.1").validate).toBe(true);
+  expect(VALIDATE_hours("20.8").validate).toBe(true);
+  expect(VALIDATE_hours("0").validate).toBe(true);
+  expect(VALIDATE_hours("1.0").validate).toBe(true);
 
-  expect(VALIDATE_hours("0ajsf.")).toBe(false);
-  expect(VALIDATE_hours("dfsjaf")).toBe(false);
-  expect(VALIDATE_hours(".")).toBe(false);
-  expect(VALIDATE_hours("")).toBe(false);
-  expect(VALIDATE_hours(" ")).toBe(false);
-  expect(VALIDATE_hours("   ")).toBe(false);
+  expect(VALIDATE_hours("0ajsf.").validate).toBe(false);
+  expect(VALIDATE_hours("dfsjaf").validate).toBe(false);
+  expect(VALIDATE_hours(".").validate).toBe(false);
+  expect(VALIDATE_hours("").validate).toBe(false);
+  expect(VALIDATE_hours(" ").validate).toBe(false);
+  expect(VALIDATE_hours("   ").validate).toBe(false);
 });
 
 test("Running Validation Tests for VALIDATE_free_form", () => {
-  expect(VALIDATE_free_form("a")).toBe(true);
-  expect(VALIDATE_free_form("wafeda")).toBe(true);
+  expect(VALIDATE_free_form("a").validate).toBe(true);
+  expect(VALIDATE_free_form("wafeda").validate).toBe(true);
 
-  expect(VALIDATE_free_form("")).toBe(false);
-  expect(VALIDATE_free_form(" ")).toBe(false);
-  expect(VALIDATE_free_form("          ")).toBe(false);
-  expect(VALIDATE_free_form("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false);
+  expect(VALIDATE_free_form("").validate).toBe(false);
+  expect(VALIDATE_free_form(" ").validate).toBe(false);
+  expect(VALIDATE_free_form("          ").validate).toBe(false);
+  expect(VALIDATE_free_form("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").validate).toBe(false);
 });
--- website/src/Components/Edit/validation.ts
+++ website/src/Components/Edit/validation.ts
@@ -1,30 +1,35 @@
-const VALIDATE_name = () => {};
+import { ValidationMessage } from "./validationMessage";
 
 /** All of the internal number validations have edge cases... this should work for our test input. Returns
  * true if the input string is a valid positive number. Function is safe regardless of input type.
  */
-const VALIDATE_hours = (hours: string) => {
-  if (typeof hours !== "string") return false;
-  if (!VALIDATE_free_form(hours)) return false;
-  if (hours === ".") return false;
+const VALIDATE_hours = (hours: string): ValidationMessage => {
+  if (typeof hours !== "string") return { validate: false, message: "Input Not String" };
+  if (!VALIDATE_free_form(hours).validate) return { validate: false, message: "Input Empty or too Long" };
+  if (hours === ".") return { validate: false, message: "Invalid Hour" };
   let dot_used: boolean = false;
   for (let i = 0; i < hours.length; i++) {
     if (hours.charAt(i) === ".") {
-      if (dot_used) return false;
+      if (dot_used) return { validate: false, message: "Invalid Hour" };
       else dot_used = true;
     } else if (
-      !(hours.charAt(i) in ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"])
+      !(hours.charAt(i) in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
     ) {
-      return false;
+      return { validate: false, message: "Invalid Hour" };
     }
   }
-  return true;
+  return { validate: true, message: "" };
 };
 
-const VALIDATE_free_form = (text: string) => {
+const VALIDATE_free_form = (text: string): ValidationMessage => {
   const trimmedText = text.trim()
-  if (trimmedText.length === 0 || trimmedText.length > 100) return false;
-  return true;
+  if (trimmedText.length === 0) {
+    return { validate: false, message: "Input Empty" };
+  }
+  else if (trimmedText.length > 100) {
+    return { validate: false, message: "Input Too Long" };
+  }
+  return { validate: true, message: "" };
 }
 
 export { VALIDATE_hours, VALIDATE_free_form };
