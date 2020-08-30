a41c4ab14ce92a4b670ca3cfb3e1736369fd9686
Daniel Sudzilouski <daniel@sudzilouski.com>
2020-08-30 11:43:11 -0700

    confirmation for sign out

--- website/src/Firebase/linkAuth/signOut.ts
+++ website/src/Firebase/linkAuth/signOut.ts
@@ -1,13 +1,18 @@
 import { firebase } from "../setup";
 
+/** Promps a user to sign out and then signs them out. Returns error code string or null if successfull */
 const signOut = async () => {
-  const result = await firebase
-    .auth()
-    .signOut()
-    .catch((error: firebase.auth.Error) => {
-      return error.message;
-    });
-  if (result) return result;
+  const confirmed = window.confirm("Are you sure that you want to sign out?");
+  if (confirmed) {
+    const result = await firebase
+      .auth()
+      .signOut()
+      .catch((error: firebase.auth.Error) => {
+        return error.message;
+      });
+    if (result) return result;
+    return null;
+  }
   return null;
 };
 
