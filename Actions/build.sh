ca6c9dd538e24084efe03b99b9a95ff4be6341ad
Daniel Sudzilouski <daniel@sudzilouski.com>
2020-08-30 20:46:05 -0700

    404

--- Actions/build.sh
+++ Actions/build.sh
@@ -2,8 +2,8 @@
 echo "starting website build, running build.sh"
 export NODE_ENV=production # makes sure that purgecss is run with the tailwind linker
 cd website
-cp public/index.html public/404.html # serves all routes as index.html so that internal routing can work
 yarn
 yarn build
+cp build/index.html build/404.html # serves all routes as index.html so that internal routing can work
 exit $? # on github-actions, errors codes are not outputed correctly for some reason
 touch build/.nojekyll # if website is server on gh-pages, disables jekyll formatting
