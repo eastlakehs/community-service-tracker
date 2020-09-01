34da6c845f613e655f1ae2f8dafb9153fcf97f4b
daniel-sudz <daniel@sudzilouski.com>
2020-09-01 13:34:41 -0700

    Update footer.tsx

--- website/src/Components/Footer/footer.tsx
+++ website/src/Components/Footer/footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center bg-top-red">
       <p className="text-white pt-2">Eastlake 2020</p>
-      <a className="text-white" href="https://repo.ehs-service.org">
-        issues/suggestion: repo.ehs-service.org
+      <a className="text-white" href="mailto:eastlakekey@gmail.com">
+        Issues/Suggestions: eastlakekey@gmail.com
       </a>
-      <a className="text-white pb-2" href="mailto:eastlakekey@gmail.com">
-        contact: eastlakekey@gmail.com
+      <a className="text-white pb-2" href="https://repo.ehs-service.org">
+        About: repo.ehs-service.org
       </a>
     </footer>
   );
