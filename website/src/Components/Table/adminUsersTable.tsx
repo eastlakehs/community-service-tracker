ca3e91211262d8fdebd4bdf094f16bd2501c18d9
Jason Zhang <zjjc123@hotmail.com>
2021-03-13 00:36:25 -0800

    Basic Search Function

--- website/src/Components/Table/adminUsersTable.tsx
+++ website/src/Components/Table/adminUsersTable.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { profileAndEmail } from "../../Firebase/firestore/getCurrentUsers"
-import InfoPage from "../Info/infoPage";
+import React, { useState } from "react";
+import { profileAndEmail } from "../../Firebase/firestore/getCurrentUsers";
+
+import { filterAdminResult } from "../../Components/Admin/search";
+
 /* Table CSS Credit: https://tailwindcomponents.com/component/table-responsive-with-filters */
 
 const TableHeaderStyle: string =
@@ -11,6 +13,7 @@ const AdminUsersTable: React.FunctionComponent<{
   handleView: (userID: string) => void;
 }> = ({ data, handleView }) => {
   const headers = ['Email', 'Name', 'Graduation Year']
+  const [filteredData, setFilteredData] = useState(data)
   const generateHeader = () => {
     return headers.map((header) => {
       return (
@@ -23,12 +26,12 @@ const AdminUsersTable: React.FunctionComponent<{
   const TableCell: React.FunctionComponent<{ name: string | number }> = ({
     name,
   }) => (
-      <td
-        className="px-2 py-2 sm:px-3 sm:py-3 md:px-4 md:py-4 lg:px-5 lg:py-5 border-b border-gray-200 bg-white text-sm"
-      >
-        <p className="text-gray-900">{name}</p>
-      </td>
-    );
+    <td
+      className="px-2 py-2 sm:px-3 sm:py-3 md:px-4 md:py-4 lg:px-5 lg:py-5 border-b border-gray-200 bg-white text-sm"
+    >
+      <p className="text-gray-900">{name}</p>
+    </td>
+  );
 
   const TableButton: React.FunctionComponent<{
     name: string;
@@ -46,9 +49,9 @@ const AdminUsersTable: React.FunctionComponent<{
   );
 
   const generateTableData = () => {
-    return data.map((user) => {
+    return filteredData.map((user) => {
       return (
-        <tr key={data ? JSON.stringify(user): "empty-entry"}>
+        <tr key={data ? JSON.stringify(user) : "empty-entry"}>
           <TableCell name={user.email} />
           <TableCell name={user.firstName + ' ' + user.lastName} />
           <TableCell name={user.graduationYear} />
@@ -66,6 +69,10 @@ const AdminUsersTable: React.FunctionComponent<{
     });
   };
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilteredData(filterAdminResult(e.target.value, data))
+  }
+
   /*
   if (Object.keys(data.data).length === 0) {
     return (
@@ -73,7 +80,7 @@ const AdminUsersTable: React.FunctionComponent<{
     )
   }
   */
- 
+
   return (
     <>
       <span className="flex justify-center text-white text-3xl">
@@ -83,17 +90,17 @@ const AdminUsersTable: React.FunctionComponent<{
         <div className="py-8">
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-            <div className="block relative">
-                    <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                        <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-500">
-                            <path
-                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
-                            </path>
-                        </svg>
-                    </span>
-                    <input placeholder="Search"
-                        className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
-                </div>
+              <div className="block relative">
+                <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-500">
+                    <path
+                      d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                    </path>
+                  </svg>
+                </span>
+                <input placeholder="Search" onChange={(e) => { handleSearch(e) }}
+                  className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+              </div>
               <table className={"min-w-full leading-normal"}>
                 <thead>
                   <tr>
