4e7a9e36d57096bfd4480a4d117fce7272ec878c
Jason Zhang <zjjc123@hotmail.com>
2021-04-27 17:42:45 -0700

    Using CSV Stringify Library

--- website/src/Components/Table/exporter/handleExport.ts
+++ website/src/Components/Table/exporter/handleExport.ts
@@ -1,17 +1,24 @@
 import { initialStateType } from "../../../Redux/userDataSlice";
 import { downloadFileFromString } from "../../../Firebase/firestore/getReport";
 import { Day } from "react-modern-calendar-datepicker";
+import stringify from "csv-stringify";
 
 const handleExport = (data: initialStateType) => {
-    let exportString = "Name,Description,Hours,Date\n";
+    let exportArray: string[][] = [];
+    exportArray.push(data.header);
     Object.keys(data.data).forEach((entry) => {
         const dateObj = JSON.parse(data.data[entry].Date) as Day;
-        exportString += `"${data.data[entry].Name}",`;
-        exportString += `"${data.data[entry].Description}",`;
-        exportString += `${data.data[entry].Hours},`;
-        exportString += `${dateObj.month}-${dateObj.day}-${dateObj.year}`  + ', ';
-        exportString += "\n";
+        let dataRow: string[] = [];
+        dataRow.push(data.data[entry].Name);
+        dataRow.push(data.data[entry].Description);
+        dataRow.push(data.data[entry].Hours);
+        dataRow.push(`${dateObj.month}-${dateObj.day}-${dateObj.year}`);
+        exportArray.push(dataRow);
     });
-    downloadFileFromString(exportString, "export_hours.csv");
+
+    stringify(exportArray, (err: any, output: string) => {
+        downloadFileFromString(output, "export_hours.csv");
+    });
+
 };
 export { handleExport };
