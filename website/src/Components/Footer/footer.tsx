72134c7bc82567042f51064a6d27f48b52c4f8db
Sam Tischaefer <samtisch@live.com>
2021-05-01 20:29:09 -0700

    This commit should make the repo more accessible, especially to people who have not contributed before.

--- website/src/Components/Footer/footer.tsx
+++ website/src/Components/Footer/footer.tsx
@@ -4,12 +4,16 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center bg-top-red">
       <p className="text-white pt-2">Eastlake 2020</p>
-      <a className="text-white" href="mailto:eastlakekey@gmail.com">
-        Issues/Suggestions: eastlakekey@gmail.com
-      </a>
-      <a className="text-white pb-2" href="https://repo.ehs-service.org">
-        About: repo.ehs-service.org
-      </a>
+      <p className="text-white pb-2">
+          This tracker is open source: <a href="https://github.com/eastlakehs/community-service-tracker">
+          https://github.com/eastlakehs/community-service-tracker
+        </a>
+      </p>
+
+    <a className="text-white" href="mailto:eastlakekey@gmail.com">
+        Questions: eastlakekey@gmail.com
+    </a>
+
     </footer>
   );
 };
