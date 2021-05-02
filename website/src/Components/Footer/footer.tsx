dbf5ddfa84d1911adb381aab62a08afc5a14f7fc
Sam Tischaefer <samtisch@live.com>
2021-05-02 00:20:54 -0700

    Make the repo more accessible (#144)
    * This commit should make the repo more accessible, especially to people who have not contributed before.
    
    * Reformat the footer
    
    Co-authored-by: daniel-sudz <daniel@sudzilouski.com>
    
    * Added link to CONTRIBUTING.md in README.md
    
    * Better URL Fix
    
    * Update footer.tsx
    
    * move to anchor tags
    
    * fixed spacing bug
    
    * change un-anchored text to <p> tag
    
    Co-authored-by: daniel-sudz <daniel@sudzilouski.com>
    Co-authored-by: Jason Zhang <zjjc123@hotmail.com>

--- website/src/Components/Footer/footer.tsx
+++ website/src/Components/Footer/footer.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center bg-top-red">
-      <p className="text-white pt-2">Eastlake 2020</p>
-      <a className="text-white" href="mailto:eastlakekey@gmail.com">
-        Issues/Suggestions: eastlakekey@gmail.com
+   <footer className="flex flex-col items-center bg-top-red">
+      <p className="text-white pt-2">Eastlake 2021</p>
+      <a className="text-white" href="https://repo.ehs-service.org">
+        This tracker is open source: contribute today!
       </a>
-      <a className="text-white pb-2" href="https://repo.ehs-service.org">
-        About: repo.ehs-service.org
+      <a className="text-white pb-2" href="mailto:eastlakekey@gmail.com">
+        Questions: eastlakekey@gmail.com
       </a>
     </footer>
   );
