2d71646e9cc6cb84470ace3584d0e9a8810c49f1
daniel-sudz <daniel@sudzilouski.com>
2021-05-02 00:08:20 -0700

    fixed spacing bug

--- website/src/Components/Footer/footer.tsx
+++ website/src/Components/Footer/footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
    <footer className="flex flex-col items-center bg-top-red">
       <a className="text-white pt-2">Eastlake 2021</a>
-      <a className="text-white pb-2" href="https://repo.ehs-service.org">
+      <a className="text-white" href="https://repo.ehs-service.org">
         This tracker is open source: contribute today!
       </a>
-      <a className="text-white" href="mailto:eastlakekey@gmail.com">
+      <a className="text-white pb-2" href="mailto:eastlakekey@gmail.com">
         Questions: eastlakekey@gmail.com
       </a>
     </footer>
