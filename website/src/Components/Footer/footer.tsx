8720a426f504ab75faa385034aa4fcbe7bf459b4
daniel-sudz <daniel@sudzilouski.com>
2021-05-02 00:03:29 -0700

    move to anchor tags

--- website/src/Components/Footer/footer.tsx
+++ website/src/Components/Footer/footer.tsx
@@ -2,18 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center bg-top-red">
-      <p className="text-white pt-2">Eastlake 2020</p>
-      <p className="text-white pb-2">
-          This tracker is open source: <a href="https://repo.ehs-service.org">
-          contribute here
-        </a>
-      </p>
-
-    <a className="text-white" href="mailto:eastlakekey@gmail.com">
+   <footer className="flex flex-col items-center bg-top-red">
+      <a className="text-white pt-2">Eastlake 2021</a>
+      <a className="text-white pb-2" href="https://repo.ehs-service.org">
+        This tracker is open source: contribute today!
+      </a>
+      <a className="text-white" href="mailto:eastlakekey@gmail.com">
         Questions: eastlakekey@gmail.com
-    </a>
-
+      </a>
     </footer>
   );
 };
