6e16f4e9acb02f21b00d7bb7230a32f5119ce98a
Jason Zhang <zjjc123@hotmail.com>
2021-05-03 16:51:27 -0700

    Recapitalizing Subfolders (#164)
    * Recapitalizing Subfolders
    
    * Update tests.yml
    
    * References Fix

---
+++ website/src/firebase/firestore/useUserProfile.ts
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { db } from "../setup";
+import { setCurrentProfile } from "../../redux/profileScreenSlice";
+import { userProfileData } from "./firestoreData.type";
+import { selectSignedInState } from "../../redux/signedInSlice";
+
+/** Handles updating the redux store with the data of the current signed in user */
+const useSyncUserProfile = () => {
+  const signedInstate = useSelector(selectSignedInState);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let unsubscribe: null | (() => void) = null;
+    if (signedInstate.signedIn) {
+      unsubscribe = db
+        .collection("users")
+        .doc(signedInstate.userEmail)
+        .collection("profile")
+        .onSnapshot((querySnapshot) => {
+          querySnapshot.docChanges().forEach((change) => {
+            const DocumentData = change.doc.data() as userProfileData;
+            if (change.type === "added" && change.doc.id === "info") {
+              console.log("added profile");
+              dispatch(setCurrentProfile(DocumentData));
+            }
+            if (change.type === "modified" && change.doc.id === "info") {
+              console.log("modified profile");
+              dispatch(setCurrentProfile(DocumentData));
+            }
+          });
+        });
+    }
+    if (unsubscribe) {
+      return unsubscribe;
+    }
+  }, [signedInstate, dispatch]);
+};
+export { useSyncUserProfile };
---
+++ website/src/pages/home.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Body from "../components/body/body";
+import Helmet from "../components/header/helmet";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectSignedInState } from "../redux/signedInSlice";
+
+const Home = () => {
+  const signedInstate = useSelector(selectSignedInState);
+  const history = useHistory();
+  // users that are logged in in should be redirected to profile page
+  useEffect(() => {
+    if (signedInstate.signedIn) {
+      history.replace("/profile");
+    }
+  });
+  return (
+    <>
+      <Helmet
+        title="EHS tracker"
+        description="Main page for the eastlake service tracker website where students can record their volunteer hours."
+      />
+      <Body />
+    </>
+  );
+};
+
+export default Home;
