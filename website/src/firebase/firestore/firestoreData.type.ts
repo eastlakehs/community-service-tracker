50322f14f26189cf9d594084c60396059fd1f767
Sam Tischaefer <samtisch@live.com>
2021-05-06 06:25:45 -0700

    Add a "notes" section to the edit page (#169)
    Adds a notes section to each entry for whatever info users want to put in there

--- website/src/firebase/firestore/firestoreData.type.ts
+++ website/src/firebase/firestore/firestoreData.type.ts
@@ -9,6 +9,7 @@ export interface firestoreDocumentType {
   NHSofficer: string;
   contactName: string;
   contactPhone: string;
+  notes: string;
 }
 
 const blankDocument: firestoreDocumentType = {
@@ -25,6 +26,7 @@ const blankDocument: firestoreDocumentType = {
   NHSofficer: "",
   contactName: "",
   contactPhone: "",
+  notes: "",
 };
 
 export interface userProfileData {
