bfaf675cf20910cebed154cae71aada96479fc1c
zjjc123 <zjjc123@hotmail.com>
2021-05-07 12:04:49 -0700

    Python License Updater

---
+++ actions/license/update.py
@@ -0,0 +1,14 @@
+import sys
+import fileinput
+from datetime import datetime
+import re
+
+today = datetime.today()
+
+with open("../../LICENSE", "r+") as f:
+    old = f.read() # read everything in the file
+    f.truncate(0)
+    f.seek(0)
+    f.write(re.sub(r"([1-3][0-9]{3})", str(today.year), old))
+
+print("Completed!")
