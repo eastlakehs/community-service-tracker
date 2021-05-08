9a9457a90565c3dd1157fb438e4ea65a31ada081
Jason Zhang <zjjc123@hotmail.com>
2021-05-08 12:20:57 -0700

    Comments

--- actions/license/update.py
+++ actions/license/update.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 import re
 
+# get today's date
 today = datetime.today()
 
 with open("../../LICENSE", "r+") as f:
-    old = f.read() # read everything in the file
+    # read everything in the file and store
+    old = f.read() 
+    # delete
     f.truncate(0)
+    # go to start
     f.seek(0)
+    # rewrite by subbing in any year related numbers with today's year
     f.write(re.sub(r"([1-3][0-9]{3})", str(today.year), old))
 
 print("Completed!")
