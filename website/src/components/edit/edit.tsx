4379be0895a057ef1592a0ffe94e87c992d8624f
Timofei Kachan <67656268+TimofeiK27@users.noreply.github.com>
2021-05-08 17:16:11 -0700

    visual changes to submit/edit page

--- website/src/components/edit/edit.tsx
+++ website/src/components/edit/edit.tsx
@@ -79,78 +79,87 @@ const Edit: React.FC<{
 
   return (
     <div className="mb-auto">
-      <form className="w-full max-w-lg container mx-auto px-4 sm:px-8 items-center">
+      <form className="w-full container max-w-lg mx-auto px-4 sm:px-8 items-center">
         <h1 className="text-center text-gray-200 py-2 lg:py-3 text-4xl sm:text-4xl lg:text-5xl xl:text-6xl">
           {editing ? "Edit your activity" : "Log a new activity"}
         </h1>
-        <StringField
-          name="Activity Name"
-          placeholder="Organization, club, etc."
-          value={currentData ? currentData.Name : ""}
-          setValue={(value: string) => {
-            dispatch(setName(value));
-          }}
-          shouldShowError={shouldShowError}
-          error={!VALIDATE_free_form(currentData?.Name).validate}
-          errorMessage={VALIDATE_free_form(currentData?.Name).message}
-        />
-        <StringField
-          name="Description"
-          placeholder="I did ....."
-          setValue={(value: string) => {
-            dispatch(setDescription(value));
-          }}
-          value={currentData ? currentData.Description : ""}
-          shouldShowError={shouldShowError}
-          error={!VALIDATE_free_form(currentData?.Description).validate}
-          errorMessage={VALIDATE_free_form(currentData?.Description).message}
-        />
+        <div className="grid grid-cols-2 gap-4">
+          <StringField
+            name="Activity Name"
+            placeholder="Organization, club, etc."
+            value={currentData ? currentData.Name : ""}
+            setValue={(value: string) => {
+              dispatch(setName(value));
+            }}
+            shouldShowError={shouldShowError}
+            error={!VALIDATE_free_form(currentData?.Name).validate}
+            errorMessage={VALIDATE_free_form(currentData?.Name).message}
+          />
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <StringField
+                name="Hours"
+                placeholder="Ex: 5"
+                value={currentData ? currentData.Hours : ""}
+                setValue={(value: string) => {
+                  dispatch(setHours(value));
+                }}
+                shouldShowError={shouldShowError}
+                error={!VALIDATE_hours(currentData?.Hours).validate}
+                errorMessage={VALIDATE_hours(currentData?.Hours).message}
+              />
+            </div>
+            <div>
+              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
+                Date
+              </label>
+              <DatePicker
+                value={currentData ? (JSON.parse(currentData.Date) as Day) : null}
+                onChange={(day) => {
+                  day && dispatch(setDate(JSON.stringify(day)));
+                }}
+                inputPlaceholder="Select a day"
+                inputClassName="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              />
 
-        <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
-          Date
-        </label>
-        <DatePicker
-          value={currentData ? (JSON.parse(currentData.Date) as Day) : null}
-          onChange={(day) => {
-            day && dispatch(setDate(JSON.stringify(day)));
-          }}
-          inputPlaceholder="Select a day"
-          inputClassName="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-        />
+            </div>
+          </div>
+          <StringField
+            name="Contact Name"
+            placeholder="Jonathan Swift"
+            value={currentData ? currentData.contactName : ""}
+            setValue={(value: string) => {
+              dispatch(setContactName(value));
+            }}
+            shouldShowError={shouldShowError}
+            error={!VALIDATE_free_form(currentData?.contactName).validate}
+            errorMessage={VALIDATE_free_form(currentData?.contactName).message}
+          />
+          <StringField
+            //This is named contact phone, but it will take any info.
+            name="Contact Phone Number or Email"
+            placeholder="(425) 123-4567"
+            value={currentData ? currentData.contactPhone : ""}
+            setValue={(value: string) => {
+              dispatch(setContactPhone(value));
+            }}
+            shouldShowError={shouldShowError}
+            error={!VALIDATE_free_form(currentData?.contactPhone).validate}
+            errorMessage={VALIDATE_free_form(currentData?.contactPhone).message}
+          />
+        </div>
         <StringField
-          name="Hours"
-          placeholder="Ex: 5"
-          value={currentData ? currentData.Hours : ""}
-          setValue={(value: string) => {
-            dispatch(setHours(value));
-          }}
-          shouldShowError={shouldShowError}
-          error={!VALIDATE_hours(currentData?.Hours).validate}
-          errorMessage={VALIDATE_hours(currentData?.Hours).message}
-        />
-        <StringField
-          name="Contact Name"
-          placeholder="Jonathan Swift"
-          value={currentData ? currentData.contactName : ""}
-          setValue={(value: string) => {
-            dispatch(setContactName(value));
-          }}
-          shouldShowError={shouldShowError}
-          error={!VALIDATE_free_form(currentData?.contactName).validate}
-          errorMessage={VALIDATE_free_form(currentData?.contactName).message}
-        />
-        <StringField
-          //This is named contact phone, but it will take any info.
-          name="Contact Phone Number or Email"
-          placeholder="(425) 123-4567"
-          value={currentData ? currentData.contactPhone : ""}
-          setValue={(value: string) => {
-            dispatch(setContactPhone(value));
-          }}
-          shouldShowError={shouldShowError}
-          error={!VALIDATE_free_form(currentData?.contactPhone).validate}
-          errorMessage={VALIDATE_free_form(currentData?.contactPhone).message}
-        />
+            name="Description"
+            placeholder="I did ....."
+            setValue={(value: string) => {
+              dispatch(setDescription(value));
+            }}
+            value={currentData ? currentData.Description : ""}
+            shouldShowError={shouldShowError}
+            error={!VALIDATE_free_form(currentData?.Description).validate}
+            errorMessage={VALIDATE_free_form(currentData?.Description).message}
+          />
+        
         <StringField
           name="Notes:"
           placeholder=""
@@ -162,20 +171,73 @@ const Edit: React.FC<{
           error={!VALIDATE_free_form(currentData?.notes, true).validate}
           errorMessage={VALIDATE_free_form(currentData?.notes, true).message}
         />
-        <CheckBox
-          label="Key Club Event"
-          setState={(value: boolean) => {
-            dispatch(setKeyClub(value ? "Yes" : "No"));
-          }}
-          checked={currentData && currentData.KeyClub === "Yes" ? true : false}
-        />
-        <CheckBox
-          label="NHS"
-          setState={(value: boolean) => {
-            dispatch(setNHS(value ? "Yes" : "No"));
-          }}
-          checked={currentData && currentData.NHS === "Yes" ? true : false}
-        />
+        <div>
+          <div className="grid grid-cols-11">
+            <div className="col-span-4">
+              <CheckBox
+                label="Key Club Event"
+                setState={(value: boolean) => {
+                  dispatch(setKeyClub(value ? "Yes" : "No"));
+                }}
+                checked={currentData && currentData.KeyClub === "Yes" ? true : false}
+              />
+            </div>
+            <div className="col-span-2">
+              <CheckBox
+                label="NHS"
+                setState={(value: boolean) => {
+                  dispatch(setNHS(value ? "Yes" : "No"));
+                }}
+                checked={currentData && currentData.NHS === "Yes" ? true : false}
+              />
+            </div>
+            <div className="col-span-5 text-right">
+              <FormSubmitButton
+                hidden={!waitingForSubmit}
+                buttonText={editing ? "Edit Activity" : "Log new Activity"}
+                onSubmit={async () => {
+                  if (!ValidateEditPage()) {
+                    setShouldShowError(true);
+                    return;
+                  }
+                  setWaitingForSubmit(true);
+                  if (
+                    editing &&
+                    typeof signedInEmail === "string" &&
+                    currentData &&
+                    currentKey
+                  ) {
+                    const resp = await submitEdit(
+                      currentData,
+                      currentKey,
+                      signedInEmail
+                    );
+                    setWaitingForSubmit(false);
+                    if (resp) {
+                      history.push("/table");
+                      toast.success("Edit submited successfully", ToastConfig);
+                      dispatch(clearCurrentEdit());
+                    } else {
+                      toast.error("Failed to submit error", ToastConfig);
+                    }
+                  }
+                  if (!editing && typeof signedInEmail === "string" && currentData) {
+                    const resp = await submitNewEntry(currentData, signedInEmail);
+                    setWaitingForSubmit(false);
+                    if (resp) {
+                      toast.success("New activity logged successfully", ToastConfig);
+                      history.push("/table");
+                      dispatch(clearCurrentEdit());
+                    } else {
+                      toast.error("Failed to log new activity", ToastConfig);
+                    }
+                  }
+                }}
+              />
+           </div>
+          </div>
+          
+        </div>
         <StringField
           name="Officer Name"
           placeholder="Smith"
@@ -187,49 +249,6 @@ const Edit: React.FC<{
           shouldShowError={shouldShowError}
           error={!VALIDATE_free_form(currentData?.NHSofficer).validate}
           errorMessage={VALIDATE_free_form(currentData?.NHSofficer).message}
-        />
-
-        <FormSubmitButton
-          hidden={!waitingForSubmit}
-          buttonText={editing ? "Edit Activity" : "Log new Activity"}
-          onSubmit={async () => {
-            if (!ValidateEditPage()) {
-              setShouldShowError(true);
-              return;
-            }
-            setWaitingForSubmit(true);
-            if (
-              editing &&
-              typeof signedInEmail === "string" &&
-              currentData &&
-              currentKey
-            ) {
-              const resp = await submitEdit(
-                currentData,
-                currentKey,
-                signedInEmail
-              );
-              setWaitingForSubmit(false);
-              if (resp) {
-                history.push("/table");
-                toast.success("Edit submited successfully", ToastConfig);
-                dispatch(clearCurrentEdit());
-              } else {
-                toast.error("Failed to submit error", ToastConfig);
-              }
-            }
-            if (!editing && typeof signedInEmail === "string" && currentData) {
-              const resp = await submitNewEntry(currentData, signedInEmail);
-              setWaitingForSubmit(false);
-              if (resp) {
-                toast.success("New activity logged successfully", ToastConfig);
-                history.push("/table");
-                dispatch(clearCurrentEdit());
-              } else {
-                toast.error("Failed to log new activity", ToastConfig);
-              }
-            }
-          }}
         />
       </form>
     </div>
