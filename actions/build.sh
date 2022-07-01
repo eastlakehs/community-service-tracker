3ade3d55b2088f2afef5bd3d6d792754476cf4a2
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-07-01 15:54:51 -0700

    set netlify node version

--- actions/build.sh
+++ actions/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "starting website build, running build.sh"
 export NODE_ENV=production # makes sure that purgecss is run with the tailwind linker
-cd website
+export NODE_VERSION=16 # https://docs.netlify.com/configure-builds/manage-dependencies/
 npm i
 npm run build
 cp build/index.html build/404.html # serves all routes as index.html so that internal routing can work
