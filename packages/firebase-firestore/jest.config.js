1b7acb275a235bbab7522349fc6bd09de1b2f171
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-07-02 13:36:23 -0700

    move firestore to seperate package (#263)
    * move firestore to seperate package
    
    * update scripts
    
    * update scripts
    
    * update scripts
    
    * format

---
+++ packages/firebase-firestore/jest.config.js
@@ -0,0 +1,9 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+export default {
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.[j]s?(x)", "**/?(*.)+(spec|test).[j]s?(x)"],
+  // disable any jest code transforms
+  transform: {},
+};
