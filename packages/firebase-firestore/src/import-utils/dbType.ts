0030fcac734a268be8c269b8083ba44ec810e0a9
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-07-06 13:03:19 -0700

    format

--- packages/firebase-firestore/src/import-utils/dbType.ts
+++ packages/firebase-firestore/src/import-utils/dbType.ts
@@ -3,25 +3,24 @@
  *  string, number, boolean, map, array, null, timestamp, geopoint, and reference
  *  we only need a subset of these
  */
-export type fieldValuePrimitive = string | number | boolean
+export type fieldValuePrimitive = string | number | boolean;
 export type fieldValueType = {
-    [key: string]: fieldValuePrimitive | fieldValueType,
+  [key: string]: fieldValuePrimitive | fieldValueType;
 };
-
 
 /**
  * dbType represents the full state of an arbitrary firestore DB
  * using a subset of the supported firestore types
  */
 export type dbType = {
-    // collections
+  // collections
+  [key: string]: {
+    // documents in collections
     [key: string]: {
-        // documents in collections
-        [key: string]: {
-            // document value
-            fieldValues: fieldValueType
-            // subcollection recursion
-            subCollections: dbType
-        }
-    }
-}
+      // document value
+      fieldValues: fieldValueType;
+      // subcollection recursion
+      subCollections: dbType;
+    };
+  };
+};
