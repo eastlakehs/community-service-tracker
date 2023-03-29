32427c9c604515a3ab2d20da211fc65a184c83e7
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-03-28 21:33:29 -0400

    update url

--- packages/firebase-firestore/test/storageRules.test.ts
+++ packages/firebase-firestore/test/storageRules.test.ts
@@ -22,6 +22,7 @@ import {
       projectId: "community-ser",
       hub: {host: "127.0.0.1",      port:4400},
       storage:{ host: "127.0.0.1",  port:5002},
+      
     });
   });
   
@@ -35,7 +36,7 @@ import {
       email: email,
       ...authContext,
     });
-    return rulesEnv.storage();
+    return rulesEnv.storage("community-ser.appspot.com");
   };
   
   const tokenValid: TokenOptions = {
@@ -67,131 +68,37 @@ import {
 
     // signed in as student-a, reading to student-b should fail
     it("reading to student-b as student-a", async () => {
-      await assertFails(getDownloadURL(ref(storage(), "users/student-b@lwsd.org")));
+      await assertFails(getDownloadURL(ref(storage(), "pictures/student-b@lwsd.org")));
     });
     // signed in as student-a, writing to student-b should fail
     it("writing to student-b as student-a", async () => {
-      const nonUserDoc = ref(storage(), "users/student-b@lwsd.org");
+      const nonUserDoc = ref(storage(), "pictures/student-b@lwsd.org");
       await assertFails(uploadString(nonUserDoc, "foobar"));
     });
     // signed in as student-a, deleting to student-b should fail
     it("deleting to student-b as student-a", async () => {
-      const nonUserDoc = ref(storage(), "users/student-b@lwsd.org");
+      const nonUserDoc = ref(storage(), "pictures/student-b@lwsd.org");
       await assertFails(deleteObject(nonUserDoc));
     });
   });
 
   
-  /*
-  describe("own docs should resolve", () => {
+  
+  describe("own storage items docs should resolve", () => {
     // signed in as student-a@lwsd.org
     const storage = () => initStorage("student-a@lwsd.org", tokenValid);
+
+    // signed in as student-a, writing to student-a should pass
+    it("writing to student-a as student-a", async () => {
+        const userDoc = ref(storage(), "pictures/student-a@lwsd.org");
+        expect(await assertSucceeds(uploadString(userDoc, "foobar")));
+    });
   
     // signed in as student-a, reading to student-a should pass
     it("reading to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertSucceeds(getDoc(nonUserDoc)));
+      const nonUserDoc = ref(storage(), "pictures/student-a@lwsd.org");
+      expect(await assertSucceeds(getDownloadURL(ref(storage(), "pictures/student-a@lwsd.org"))));
     });
-    // signed in as student-a, writing to student-a should pass
-    it("writing to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertSucceeds(setDoc(nonUserDoc, {})));
-    });
-  });
-  */
-   /*
-  
-  describe("own docs not resolve with non-verified email", () => {
-    // signed in as student-a@lwsd.org
-    const storage = () => initStorage("student-a@lwsd.org", tokenNotVerified);
-  
-    // signed in as student-a, reading to student-a should fail with non-verified email
-    it("reading to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertFails(getDoc(nonUserDoc)));
-    });
-    // signed in as student-a, writing to student-a should fail with non-verified email
-    it("writing to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertFails(setDoc(nonUserDoc, {})));
-    });
-  });
-  
-  describe("own docs not resolve with non-password-based auth", () => {
-    // signed in as student-a@lwsd.org
-    const storage = () => initStorage("student-a@lwsd.org", tokenNotPasswordAuth);
-  
-    // signed in as student-a, reading to student-a should fail with non-password-based auth
-    it("reading to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertFails(getDoc(nonUserDoc)));
-    });
-    // signed in as student-a, writing to student-a should fail with non-password-based auth
-    it("writing to student-a as student-a", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@lwsd.org");
-      expect(await assertFails(setDoc(nonUserDoc, {})));
-    });
-  });
-  
-  describe("non valid school email docs should reject", () => {
-    // signed in as student-a@lwsd.org
-    const storage = () => initStorage("student-a@domain.org", tokenValid);
-  
-    // non valid school email read should reject
-    it("reading to student-a as student-a (@domain.org)", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@domain.org");
-      expect(await assertFails(getDoc(nonUserDoc)));
-    });
-    // non lwsd school email write should reject
-    it("writing to student-a as student-a (@domain.org)", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@domain.org");
-      expect(await assertFails(setDoc(nonUserDoc, {})));
-    });
-  });
-  
-  describe("bellevue college student emails should pass", () => {
-    // signed in as student-a@bellevuecollege.edu
-    const storage = () => initStorage("student-a@bellevuecollege.edu", tokenValid);
-  
-    // whitelisted non lwsd email read should pass
-    it("reading to student-a as student-a (@bellevuecollege.edu)", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@bellevuecollege.edu");
-      expect(await assertSucceeds(getDoc(nonUserDoc)));
-    });
-    // whitelisted non lwsd email write should pass
-    it("writing to student-a as student-a (@bellevuecollege.edu)", async () => {
-      const nonUserDoc = doc(db(), "users/student-a@bellevuecollege.edu");
-      expect(await assertSucceeds(setDoc(nonUserDoc, {})));
-    });
-  });
-  
-  describe("admins should be able to do anything", () => {
-    // signed in as admin (eastlakekey@gmail.com)
-    const storage = () => initStorage("eastlakekey@gmail.com", tokenValid);
-  
-    const randomDoc1 = () => doc(db(), "users/dsaklk");
-    const randomDoc2 = () => doc(db(), "dsagafdsa/321fdssa");
-    const randomDoc3 = () => doc(db(), "ddsadsa/fsakl.dsa/dsak.k.dfsa/dsads.kvk");
-  
-    it("reading to anything as admin", async () => {
-      expect(await assertSucceeds(getDoc(randomDoc1())));
-      expect(await assertSucceeds(getDoc(randomDoc2())));
-      expect(await assertSucceeds(getDoc(randomDoc3())));
-    });
-  
-    it("writing to anything as admin", async () => {
-      expect(await assertSucceeds(setDoc(randomDoc1(), {})));
-      expect(await assertSucceeds(setDoc(randomDoc2(), {})));
-      expect(await assertSucceeds(setDoc(randomDoc3(), {})));
-    });
-  
-    it("deleting anything as admin", async () => {
-      expect(await assertSucceeds(deleteDoc(randomDoc1())));
-      expect(await assertSucceeds(deleteDoc(randomDoc2())));
-      expect(await assertSucceeds(deleteDoc(randomDoc3())));
-    });
-  });
-  */
- 
 
+  });
   
